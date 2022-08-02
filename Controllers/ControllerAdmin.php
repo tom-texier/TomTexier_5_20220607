518f865d605ad450df8186c6fb47a153f28eded2
tom-texier <tom.texierpro@gmail.com>
2022-08-02 15:46:25 +0200

    postsManagement - PostList

--- Controllers/ControllerAdmin.php
+++ Controllers/ControllerAdmin.php
@@ -1,11 +1,36 @@
 <?php
 
+use App\Model\Manager\CommentsManager;
+use App\Model\Manager\PostsManager;
+use App\Model\Manager\UsersManager;
 use Texier\Framework\ControllerSecured;
 
 class ControllerAdmin extends ControllerSecured
 {
+    private PostsManager $postsManager;
+    private CommentsManager $commentsManager;
+    private UsersManager $usersManager;
+
+    public function __construct()
+    {
+        $this->postsManager = new PostsManager();
+        $this->commentsManager = new CommentsManager();
+        $this->usersManager = new UsersManager();
+    }
+
     public function index()
     {
         $this->generateView();
+    }
+
+    public function postsManagement()
+    {
+        $numberPosts = $this->postsManager->count();
+        $posts = $this->postsManager->getList();
+
+        $this->generateView([
+            'posts'         => $posts,
+            'numberPosts'   => $numberPosts
+        ]);
     }
 }
--- Model/Manager/PostsManager.php
+++ Model/Manager/PostsManager.php
@@ -2,14 +2,38 @@
 
 namespace App\Model\Manager;
 
+use App\Model\Entity\Post;
 use Texier\Framework\Model;
-use Model\Entity\Post;
 
 class PostsManager extends Model
 {
+    /**
+     * Obtenir la liste des posts
+     * @return Post[]
+     */
     public function getList(): array
     {
+        $sql = "SELECT
+                    p.id,
+                    p.title,
+                    p.content,
+                    p.image,
+                    p.createdAt,
+                    p.updatedAt,
+                    u.username,
+                    u.email
+                FROM posts p, users u
+                WHERE p.authorId = u.id
+                ORDER BY p.createdAt DESC";
 
+        $result = $this->executeRequest($sql);
+        $posts = [];
+
+        while($row = $result->fetch(\PDO::FETCH_ASSOC)) {
+            $posts[] = new Post($row);
+        }
+
+        return $posts;
     }
 
     public function get(int $id): Post
@@ -30,6 +54,14 @@ class PostsManager extends Model
     public function update(Post $post)
     {
         
+    }
+
+    public function count()
+    {
+        $sql = "SELECT COUNT(*) as numberPosts FROM posts";
+        $result = $this->executeRequest($sql)->fetch();
+
+        return $result['numberPosts'];
     }
 }
 
