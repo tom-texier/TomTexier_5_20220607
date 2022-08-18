634b910b54b09299afa3c4df75f56f04fecb4290
tom-texier <tom.texierpro@gmail.com>
2022-08-18 17:48:45 +0200

    Update oc_blog_mvc.sql

--- datas/oc_blog_mvc.sql
+++ datas/oc_blog_mvc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Aug 10, 2022 at 05:55 PM
+-- Generation Time: Aug 18, 2022 at 05:47 PM
 -- Server version: 5.7.34
 -- PHP Version: 7.4.21
 
@@ -26,9 +26,34 @@ USE `oc_blog_mvc`;
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `author` int(11) NOT NULL,
+  `post` int(11) NOT NULL,
+  `content` text NOT NULL,
+  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` int(11) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`id`, `author`, `post`, `content`, `createdAt`, `status`) VALUES
+(4, 4, 1, 'Test commentaire', '2022-08-16 18:55:13', 2),
+(5, 4, 1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam auctor tortor tortor, condimentum hendrerit ligula porttitor ac. Aliquam arcu neque, euismod mattis nunc in, aliquam tristique ligula. Mauris est arcu, faucibus vel metus ut, feugiat hendrerit velit. Morbi fringilla faucibus dui, non fermentum orci hendrerit at. Suspendisse placerat viverra vulputate.', '2022-08-17 18:45:57', 2);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `posts`
 --
 
+DROP TABLE IF EXISTS `posts`;
 CREATE TABLE `posts` (
   `id` int(11) NOT NULL,
   `author` int(11) NOT NULL,
@@ -44,7 +69,7 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`id`, `author`, `title`, `content`, `image`, `createdAt`, `updatedAt`) VALUES
-(1, 4, 'Test article', 'Test contenu', 'post-62f1f573978b49.03112832.jpeg', '2022-08-09 07:49:39', '2022-08-09 07:59:54');
+(1, 4, 'Test article', 'Test contenu', 'post-62f1f573978b49.03112832.jpeg', '2022-08-09 07:49:39', '2022-08-16 10:20:37');
 
 -- --------------------------------------------------------
 
@@ -52,6 +77,7 @@ INSERT INTO `posts` (`id`, `author`, `title`, `content`, `image`, `createdAt`, `
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
@@ -67,12 +93,17 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `role`, `createdAt`) VALUES
 (4, 'admin', 'admin@admin.fr', '$2y$10$qRsNSWIQXPwDn0kI8QxnI.MMG4gwqPPL51hzgpQTZ5z1GGjUQfVMa', 2, '2022-07-26 11:40:34'),
-(6, 'ttexier', 'tom.texierpro@gmail.com', '$2y$10$VdO.zji2l93/ye5LTCLNSehNJq5ZdjzukR0QMXjGbFDiA.5jKd/Fm', 1, '2022-08-08 19:35:57'),
-(8, 'evzevr', 'tom.texier49@gmail.com', '$2y$10$cRkaLlYaXCEuNVRzCsyJrulmti5HJNNDGKVqR5L/6.vQtXuk8jQnu', 2, '2022-08-09 17:55:44');
+(6, 'ttexier', 'tom.texierpro@gmail.com', '$2y$10$VdO.zji2l93/ye5LTCLNSehNJq5ZdjzukR0QMXjGbFDiA.5jKd/Fm', 1, '2022-08-08 19:35:57');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `posts`
@@ -93,6 +124,12 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
@@ -102,7 +139,7 @@ ALTER TABLE `posts`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
